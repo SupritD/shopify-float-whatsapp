798b43b118e9739de7d677069d4caff45636a537
SupritD <supritdagade77@gmail.com>
2026-09-01 17:48:36 +0530

    feat: add YouTube, Phone, Email, and WeChat channels to default list with custom inputs

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -46,7 +46,11 @@ const DEFAULT_ICONS = {
   whatsapp: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.888-.788-1.487-1.761-1.663-2.06-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.885m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z" /></svg>',
   messenger: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M12 0C5.373 0 0 4.974 0 11.111c0 3.498 1.744 6.614 4.469 8.654V24l4.088-2.242c1.092.3 2.246.464 3.443.464 6.627 0 12-4.974 12-11.111C24 4.974 18.627 0 12 0zm1.191 14.963l-3.056-3.26-5.963 3.26 6.554-6.962 3.13 3.259 5.887-3.259-6.552 6.962z"/></svg>',
   instagram: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/></svg>',
-  x: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>'
+  x: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>',
+  youtube: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>',
+  phone: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M19.38 16.03a11.16 11.16 0 01-3.66-.62c-.37-.12-.79-.03-1.07.25l-2.2 2.2c-2.83-1.44-5.15-3.75-6.59-6.59l2.2-2.2c.28-.28.36-.67.25-1.02a11.12 11.12 0 01-.63-3.68C7.62 3.2 6.78 2.37 5.75 2.37H3.34c-1.03 0-1.89.87-1.82 1.9A19.98 19.98 0 0020.15 22.8c1.03.07 1.9-.8 1.9-1.83v-2.42c0-1.02-.83-1.87-1.85-1.87h-.82z"/></svg>',
+  email: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5-8-5h16zm0 12H4V8l8 5 8-5v10z"/></svg>',
+  wechat: '<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/24/svg" fill="currentColor"><path d="M11.136 6.632c0-.113-.01-.225-.03-.334C10.772 3.498 8.01 1.25 4.675 1.25 2.094 1.25 0 3.167 0 5.568c0 1.295.597 2.476 1.572 3.33.153.136.2.35.127.546l-.506 1.348 1.487-.714a.75.75 0 0 1 .632-.016c.45.2.939.317 1.454.331a3.9 3.9 0 0 1-.09-.817c0-2.4 2.1-4.346 4.686-4.346a5.05 5.05 0 0 1 1.776.326ZM4.872 3.926c.433 0 .783.315.783.704 0 .388-.35.703-.783.703-.434 0-.783-.315-.783-.703 0-.389.349-.704.783-.704Zm-2.395 0c.434 0 .784.315.784.704 0 .388-.35.703-.784.703-.433 0-.783-.315-.783-.703 0-.389.35-.704.783-.704ZM16 10.354c0-1.89-1.656-3.424-3.696-3.424-2.04 0-3.695 1.534-3.695 3.424 0 1.89 1.655 3.424 3.695 3.424.436 0 .858-.073 1.254-.207l1.173.563-.399-1.064a3.15 3.15 0 0 0 1.243-2.632c0-.028 0-.056-.002-.085Zm-4.93-1.077c.333 0 .604.249.604.555 0 .307-.271.555-.605.555-.333 0-.604-.248-.604-.555 0-.306.27-.555.604-.555Zm2.468 0c.334 0 .605.249.605.555 0 .307-.271.555-.605.555-.333 0-.604-.248-.604-.555 0-.306.271-.555.604-.555Z"/></svg>'
 };
 
 const countryDataMap = countryData.reduce((acc: any, curr: any) => {
@@ -207,6 +211,10 @@ export default function Index() {
     { id: 'messenger', name: 'Facebook Messenger', detail: 'm.me/yourbrand', icon: DEFAULT_ICONS.messenger, useDefaultIcon: true, active: true, type: 'messenger', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#0084ff", textColor: "#ffffff" } },
     { id: 'instagram', name: 'Instagram', detail: 'Not configured', icon: DEFAULT_ICONS.instagram, useDefaultIcon: true, active: false, type: 'instagram', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#E1306C", textColor: "#ffffff" } },
     { id: 'x', name: 'X (Twitter)', detail: 'twitter.com/yourbrand', icon: DEFAULT_ICONS.x, useDefaultIcon: true, active: false, type: 'x', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#000000", textColor: "#ffffff" } },
+    { id: 'youtube', name: 'YouTube', detail: 'youtube.com/@yourbrand', icon: DEFAULT_ICONS.youtube, useDefaultIcon: true, active: false, type: 'youtube', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#FF0000", textColor: "#ffffff" } },
+    { id: 'phone', name: 'Phone Call', detail: '+15551234567', icon: DEFAULT_ICONS.phone, useDefaultIcon: true, active: false, type: 'phone', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#34B7F1", textColor: "#ffffff" } },
+    { id: 'email', name: 'Email Support', detail: 'support@yourbrand.com', icon: DEFAULT_ICONS.email, useDefaultIcon: true, active: false, type: 'email', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#EA4335", textColor: "#ffffff" } },
+    { id: 'wechat', name: 'WeChat', detail: 'your_wechat_id', icon: DEFAULT_ICONS.wechat, useDefaultIcon: true, active: false, type: 'wechat', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#07C160", textColor: "#ffffff" } },
     { id: 'custom1', name: 'Custom Link', customName: 'Help Center', detail: 'https://example.com/help', icon: '🔗', useDefaultIcon: false, active: true, type: 'custom', appearance: { iconWidth: "28", iconHeight: "28", transparentBg: false, bgColor: "#000000", textColor: "#ffffff" } },
   ]);
 
@@ -907,6 +915,47 @@ export default function Index() {
                   onChange={(v) => updateChannelField(editingChannel.id, 'detail', v)}
                   autoComplete="off"
                   helpText="Example: twitter.com/yourbrand or x.com/yourbrand"
+                />
+              )}
+
+              {editingChannel.type === 'youtube' && (
+                <TextField
+                  label="YouTube Channel URL"
+                  value={editingChannel.detail}
+                  onChange={(v) => updateChannelField(editingChannel.id, 'detail', v)}
+                  autoComplete="off"
+                  helpText="Example: youtube.com/@yourbrand"
+                />
+              )}
+
+              {editingChannel.type === 'phone' && (
+                <TextField
+                  label="Phone Number"
+                  value={editingChannel.detail}
+                  onChange={(v) => updateChannelField(editingChannel.id, 'detail', v)}
+                  autoComplete="off"
+                  helpText="Example: +1 555 123 4567"
+                />
+              )}
+
+              {editingChannel.type === 'email' && (
+                <TextField
+                  label="Email Address"
+                  type="email"
+                  value={editingChannel.detail}
+                  onChange={(v) => updateChannelField(editingChannel.id, 'detail', v)}
+                  autoComplete="off"
+                  helpText="Example: support@yourbrand.com"
+                />
+              )}
+
+              {editingChannel.type === 'wechat' && (
+                <TextField
+                  label="WeChat ID"
+                  value={editingChannel.detail}
+                  onChange={(v) => updateChannelField(editingChannel.id, 'detail', v)}
+                  autoComplete="off"
+                  helpText="Enter your WeChat ID"
                 />
               )}
 
